d04b323ca24fa0e8d60e0f64c79cc6e9f4d118f0
Greg Olmschenk <golmschenk@gmail.com>
2019-05-09 15:11:45 -0400

    Add scalar data frame from log

--- log.py
+++ log.py
@@ -2,12 +2,15 @@
 import glob
 import os
 from typing import List
+import pandas as pd
+import tensorflow as tf
 
 
 class Log:
     """A class for reading TensorBoard logs."""
-    def __init__(self):
-        pass
+    def __init__(self, event_file_path: str):
+        self.event_file_path: str = event_file_path
+        self.scalars_data_frame: pd.DataFrame = self.data_frame_from_event_file_scalar_summaries(self.event_file_path)
 
     @classmethod
     def find_log_paths(cls, logs_directory) -> List[str]:
@@ -15,3 +18,15 @@ class Log:
         glob_string = os.path.join(logs_directory, '**', 'events.out.tfevents*')
         event_file_names = glob.glob(glob_string, recursive=True)
         return event_file_names
+
+    @staticmethod
+    def data_frame_from_event_file_scalar_summaries(event_file_path: str) -> pd.DataFrame:
+        """Creates a Pandas data frame from a scalar summaries event file."""
+        summary_iterator = tf.compat.v1.train.summary_iterator(event_file_path)
+        scalars_data_frame = pd.DataFrame()
+        for event in summary_iterator:
+            event_step = int(event.step)
+            for value in event.summary.value:
+                scalars_data_frame.at[event_step, value.tag] = value.simple_value
+        scalars_data_frame.sort_index(inplace=True)
+        return scalars_data_frame
--- test_log_reader.py
+++ test_log_reader.py
@@ -1,14 +1,19 @@
 """Tests for Log."""
 
 from log import Log
+import math
 
 
 class TestLog:
     """Tests for Log."""
     def test_can_find_all_logs_in_directory(self):
         log_paths = Log.find_log_paths('test_resources/example_logs')
-        assert len(log_paths) == 12
+        assert len(log_paths) == 14
         assert any('norm_mean abs_plus_one_log_mean_neg' in log_path and 'GAN' in log_path
                    for log_path in log_paths)
         assert not any('DGGAN' in log_path for log_path in log_paths)
 
+    def test_can_retrieve_scalar_data_frame(self):
+        log = Log('test_resources/example_logs/example_log/DNN/events.out.tfevents.test')
+        assert '2_Train_Error/MAE' in log.scalars_data_frame.columns
+        assert math.isclose(log.scalars_data_frame['2_Train_Error/MAE'].iloc[0], 0.0297654513)
